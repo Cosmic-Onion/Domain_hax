c87f5e2ef01fb476b03393134301ccc0aa8101af
cosmic-onion <marmaladeking@gmail.com>
2020-05-11 12:56:38 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Now you can have that shiny vanity page you always wanted!!
+### Now you can have that shiny vanity page you always wanted!
+
+[domain_hax](domain_hax2.csv) file contains output of script, contains duplicates
+
+[words](https://github.com/dwyl/english-words)
--- generate_urls.py
+++ generate_urls.py
@@ -6,18 +6,18 @@ def check_suffix(tld, word):
     return re.search(tld,word)
 
 with open("tld_e.csv") as tld_list:
-            tld_reader = csv.reader(tld_list,delimiter=',')
-            for tld_row in tld_reader:
-                with open("words.csv") as wordList:
-                    word_reader = csv.reader(wordList,delimiter=',')
-                    for word_row in word_reader:
-                        match = check_suffix(tld_row[0],word_row[1])
-                        if match:
-                            match_pos = match.start()
-                            URL = word_row[1][0:match_pos] + "." + tld_row[0]
-                            print(URL)
-                            with open("domain_hax69.csv", "a") as output:
-                                output_writer = csv.writer(output, delimiter=",")
-                                output_writer.writerow([URL])
+    tld_reader = csv.reader(tld_list,delimiter=',')
+    for tld_row in tld_reader:
+        with open("words.csv") as wordList:
+            word_reader = csv.reader(wordList,delimiter=',')
+            for word_row in word_reader:
+                match = check_suffix(tld_row[0],word_row[1])
+                if match:
+                    match_pos = match.start()
+                    URL = word_row[1][0:match_pos] + "." + tld_row[0]
+                    print(URL)
+                    with open("domain_haxNew.csv", "a") as output:
+                        output_writer = csv.writer(output, delimiter=",")
+                        output_writer.writerow([URL])
 
                 
